391caebd87e583147658a9b439c52f115e08edf3
Vladislav <double-vh@yandex.ru>
2019-08-07 19:41:29 +0600

    new Database methods

--- run.py
+++ run.py
@@ -22,27 +22,39 @@ class Database:
 		self.connection = None
 		self.cursor = None
 
-if __name__ == '__main__':
-	if '--' in sys.argv: 
-		db = Database(sys.argv[sys.argv.index('--') + 1])
-	elif '++' in sys.argv:
-		db = Database(sys.argv[sys.argv.index('++') + 1])
-		db.connect()
-		db.execute("CREATE TABLE `words` ( `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `word1` TEXT NOT NULL, `word2` TEXT NOT NULL, `lvl` INTEGER NOT NULL )")
-		db.commit()
-		db.disconnect()
+	def execute_many(self, expressions):
+		for expression in expressions:
+			self.execute(expression)
 
-	if sys.argv[1].lower() == 'add':
-		try:
+	def safe_execute_many(self, expressions):
+		self.connect()
+		self.execute_many(expressions)
+		self.commit()
+		self.disconnect()
+
+	def safe_execute(self, expression):
+		self.connect()
+		self.execute(expression)
+		self.commit()
+		self.disconnect()
+
+if __name__ == '__main__':
+	try:
+		if '--' in sys.argv: 
+			db = Database(sys.argv[sys.argv.index('--') + 1])
+		elif '++' in sys.argv:
+			db = Database(sys.argv[sys.argv.index('++') + 1])
+			db.safe_execute("CREATE TABLE `words` ( `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `word1` TEXT NOT NULL, `word2` TEXT NOT NULL, `lvl` INTEGER NOT NULL )")
+
+		if sys.argv[1].lower() == 'add':
 			w1 = sys.argv[2].lower()
 			w2 = sys.argv[3].lower()
 
-			db.connect()
-			db.execute(f"INSERT INTO words (word1, word2, lvl) VALUES (\"{w1}\", \"{w2}\", 1);")
-			db.execute(f"INSERT INTO words (word1, word2, lvl) VALUES (\"{w2}\", \"{w1}\", 1);")
-			db.commit()
-			db.disconnect()
+			db.safe_execute_many([
+				f"INSERT INTO words (word1, word2, lvl) VALUES (\"{w1}\", \"{w2}\", 1);",
+				f"INSERT INTO words (word1, word2, lvl) VALUES (\"{w2}\", \"{w1}\", 1);"
+			])
 
 			print("OK")
-		except Exception as e:
-			print(e)
+	except Exception as e:
+		print(e)
