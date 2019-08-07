506747ac058c1bcedec0351d5db38adb092ec4c8
Vladislav <double-vh@yandex.ru>
2019-08-07 19:50:04 +0600

    add callbacks

--- run.py
+++ run.py
@@ -38,23 +38,38 @@ class Database:
 		self.commit()
 		self.disconnect()
 
+class Callbacks:
+	def on_existing_db():
+		global db
+		db = Database(sys.argv[sys.argv.index('--') + 1])
+
+	def on_new_db():
+		global db
+		db = Database(sys.argv[sys.argv.index('++') + 1])
+		db.safe_execute("CREATE TABLE `words` ( `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `word1` TEXT NOT NULL, `word2` TEXT NOT NULL, `lvl` INTEGER NOT NULL )")
+
+	def on_add():
+		global db
+
+		w1 = sys.argv[2].lower()
+		w2 = sys.argv[3].lower()
+
+		db.safe_execute_many([
+			f"INSERT INTO words (word1, word2, lvl) VALUES (\"{w1}\", \"{w2}\", 1);",
+			f"INSERT INTO words (word1, word2, lvl) VALUES (\"{w2}\", \"{w1}\", 1);"
+		])
+
+		print("OK")
+
 if __name__ == '__main__':
 	try:
 		if '--' in sys.argv: 
-			db = Database(sys.argv[sys.argv.index('--') + 1])
+			Callbacks.on_existing_db()
 		elif '++' in sys.argv:
-			db = Database(sys.argv[sys.argv.index('++') + 1])
-			db.safe_execute("CREATE TABLE `words` ( `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `word1` TEXT NOT NULL, `word2` TEXT NOT NULL, `lvl` INTEGER NOT NULL )")
+			Callbacks.on_new_db()
 
 		if sys.argv[1].lower() == 'add':
-			w1 = sys.argv[2].lower()
-			w2 = sys.argv[3].lower()
-
-			db.safe_execute_many([
-				f"INSERT INTO words (word1, word2, lvl) VALUES (\"{w1}\", \"{w2}\", 1);",
-				f"INSERT INTO words (word1, word2, lvl) VALUES (\"{w2}\", \"{w1}\", 1);"
-			])
-
-			print("OK")
+			Callbacks.on_add()
+			
 	except Exception as e:
 		print(e)
