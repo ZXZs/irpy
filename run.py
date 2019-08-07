8218b489e4636823e6a75bc54c3e07e7af839282
Vladislav <double-vh@yandex.ru>
2019-08-07 19:01:41 +0600

    add "add" command

--- run.py
+++ run.py
@@ -21,3 +21,23 @@ class Database:
 		self.connection.close()
 		self.connection = None
 		self.cursor = None
+
+	def safe_execute(expression)
+
+if __name__ == '__main__':
+	db = Database('db.db')
+
+	if sys.argv[1] == 'add':
+		try:
+			w1 = sys.argv[2].lower()
+			w2 = sys.argv[3].lower()
+
+			db.connect()
+			db.execute(f"INSERT INTO words (word1, word2, lvl) VALUES (\"{w1}\", \"{w2}\", 1);")
+			db.execute(f"INSERT INTO words (word1, word2, lvl) VALUES (\"{w2}\", \"{w1}\", 1);")
+			db.commit()
+			db.disconnect()
+
+			print("OK")
+		except Exception as e:
+			print(e)
