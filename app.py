11b949d99576ef0ad4eb4b88a25f0a55504a54bd
Vladislav <double-vh@yandex.ru>
2019-08-16 16:45:34 +0600

    base web gui

--- app.py
+++
@@ -1,3 +0,0 @@
-class App:
-	def __init__(self):
-		self.db = None
--- run.py
+++ run.py
@@ -1,15 +1,47 @@
-from callbacks import Callbacks, on, if_arg_is
-from app import App
+from flask import Flask, render_template
+from dt import DateTime	
+from database import Database
 
-if __name__ == '__main__':
-	try:
-		app = App()
+flask = Flask(__name__)
 
-		if_arg_is('--').do(lambda: Callbacks.on_existing_db(app))
-		if_arg_is('++').do(lambda: Callbacks.on_new_db(app))
+db = None
 
-		on('add').do(lambda: Callbacks.on_add(app))
-		on('repeat').do(lambda: Callbacks.on_repeat(app))
-			
-	except Exception as e:
-		print(e)
+@flask.route('/')
+def index():
+	return render_template('index.html')
+
+@flask.route('/db/<name>')
+def new_db(name):
+	global db
+
+	db = Database(name)
+
+	db.safe_execute("""
+		CREATE TABLE IF NOT EXISTS `words` ( 
+			`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, 
+			`word1` TEXT NOT NULL, 
+			`word2` TEXT NOT NULL, 
+			`lvl` INTEGER NOT NULL, 
+			`timestamp` TEXT NOT NULL 
+		)
+		""")
+
+	return "OK"
+
+@flask.route('/add/<word1>/<word2>')
+def add(word1, word2):
+	global db
+
+	word1 = word1.lower()
+	word2 = word2.lower()
+
+	db.safe_execute_many([
+		f"INSERT INTO words (word1, word2, lvl, timestamp) VALUES (\"{word1}\", \"{word2}\", 1, \"{DateTime.now().__str__()}\");",
+		f"INSERT INTO words (word1, word2, lvl, timestamp) VALUES (\"{word2}\", \"{word1}\", 1, \"{DateTime.now().__str__()}\");"
+	])
+
+	return "OK"
+
+@flask.route('/repeat')
+def repeat():
+	return "OK"
