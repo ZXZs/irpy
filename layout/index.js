d5f3a9b8000d879a06324e96cdb0341fcf3474c7
Vladislav <double-vh@yandex.ru>
2019-08-27 12:34:39 +0600

    fixs

--- layout/index.js
+++ layout/index.js
@@ -1,16 +1,37 @@
 $(document).ready(function() { 
-	$('body').bootstrapMaterialDesign(); 
-
 	$('#words_submit').click(function() {
 		word1 = $('#word1').val()
 		word2 = $('#word2').val()
 		theme = $('#theme').val()
+		host  = $(location).attr('hostname') + ':5000'
 
-		$.ajax(`http://localhost:5000/add/${word1}/${word2}/${theme}`)
+		$.ajax({
+			type: 'GET',
+			url: `http://${host}/add/${word1}/${word2}/${theme}`,
+			async: true,
+			success: function(text) {
+				if (text == "OK") 
+					M.toast({html: 'Success!'})
+				else
+					M.toast({html: 'Fail!'})
+			}
+		})
 	})
 
 	$('#db_submit').click(function() {
-		db = $('#db').val()
-		$.ajax(`http://localhost:5000/db/${db}`)
+		db   = $('#db').val()
+		host = $(location).attr('hostname') + ':5000'
+
+		$.ajax({
+			type: 'GET',
+			url: `http://${host}/db/${db}`,
+			async: true,
+			success: function(text) {
+				if (text == "OK") 
+					M.toast({html: 'Success!'})
+				else
+					M.toast({html: 'Fail!'})
+			}
+		})
 	})
 })
